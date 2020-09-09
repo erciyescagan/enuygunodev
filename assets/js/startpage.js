8646d61cbda23293de27a214ca660126c0029591
isocum <isocum@gmail.com>
2020-09-09 14:16:17 +0200

    Frontend işlemleri
    JS ok

---
+++ assets/js/startpage.js
@@ -0,0 +1,10 @@
+$( "#inputCompany" ).change(function() {
+    $( "#btnAssign" ).href = "/startpage/assigndevs/company/" + $(this).val()+ "/project/" + $( "#inputProject" ).value ;
+    $( "#btnReport" ).href = "/resultpage/index/project/" + $( "#inputProject" ).value ;
+});
+
+
+$( "#inputProject" ).change(function() {
+    $( "#btnAssign" ).href = "/startpage/assigndevs/company/" + $( "#inputCompany" ).val() + "/project/" + $(this).value;
+    $( "#btnReport" ).href = "/resultpage/index/project/" + $(this).value;
+});
